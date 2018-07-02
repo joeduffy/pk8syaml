29c94f483d6f7d0f1df26c486337f84719c7d4f1
joeduffy <joe@pulumi.com>
2018-07-02 06:19:11 -0700

    Support simple template replacements

--- lib/index.ts
+++ lib/index.ts
@@ -2,12 +2,13 @@ import * as pulumi from "@pulumi/pulumi";
 import * as fs from "mz/fs";
 import * as fspath from "path";
 import * as process from "process";
+import * as traverse from "traverse";
 import * as yaml from "js-yaml";
 
 // apply simply takes a list of paths to Kubernetes YAML/JSON files, parses each -- in the order specified -- and
 // creates a set of Pulumi objects from these resources.  Because the semantics of Kubernetes configuration is that
 // resources are created in-order, all resource creation is serialized, one after the other.
-export async function apply(...paths: string[]): Promise<pulumi.Resource[]> {
+export async function apply(paths: string[], replmap?: {[key: string]: Object}): Promise<pulumi.Resource[]> {
     let nodes = [];
     for (let path of paths) {
         const file = await fs.readFile(path, "utf-8");
@@ -17,39 +18,63 @@ export async function apply(...paths: string[]): Promise<pulumi.Resource[]> {
             nodes.push(yaml.safeLoad(file));
         }
     }
-    return applyObjects(nodes);
+    return applyObjects(nodes, replmap);
 }
 
 // applyJSONs takes a list of Kubernetes JSON files as in-memory blobs of text, parses each -- in the order specified
 // -- and creates a set of Pulumi objects from these resources.  Because the semantics of Kubernetes configuration is
 // that resources are created in-order, all resource creation is serialized, one after the other.
-export function applyJSONs(...files: string[]): pulumi.Resource[] {
+export function applyJSONs(files: string[], replmap?: {[key: string]: Object}): pulumi.Resource[] {
     let nodes = [];
     for (let file of files) {
         nodes.push(JSON.parse(file));
     }
-    return applyObjects(nodes);
+    return applyObjects(nodes, replmap);
 }
 
 // applyYAMLs takes a list of Kubernetes YAML files as in-memory blobs of text, parses each -- in the order specified
 // -- and creates a set of Pulumi objects from these resources.  Because the semantics of Kubernetes configuration is
 // that resources are created in-order, all resource creation is serialized, one after the other.
-export function applyYAMLs(...files: string[]): pulumi.Resource[] {
+export function applyYAMLs(files: string[], replmap?: {[key: string]: Object}): pulumi.Resource[] {
     let nodes = [];
     for (let file of files) {
         nodes.push(yaml.safeLoad(file));
     }
-    return applyObjects(nodes);
+    return applyObjects(nodes, replmap);
 }
 
 // applyObjects takes a list of Kubernetes configuration objects and creates a set of Pulumi objects from these
 // resources.  Because the semantics of Kubernetes configuration is that resources are created in-order, all
 // resource creation is serialized, one object after the other, meaning there will be no parallelism.
-export function applyObjects(...nodes: any[]): pulumi.Resource[] {
+export function applyObjects(nodes: any[], replmap?: {[key: string]: Object}): pulumi.Resource[] {
     let resources: pulumi.Resource[] = [];
     let prior: pulumi.Resource | undefined;
     for (let node of nodes) {
         if (node) {
+            // If there's a replacement map, use it to substitute elements of the tree.
+            if (replmap) {
+                node = traverse(node).map(function (v: any) {
+                    if (typeof v === "string") {
+                        for (let k of Object.keys(replmap)) {
+                            const templ = `{{${k}}}`;
+                            const replw = replmap[k];
+                            if (v === templ) {
+                                this.update(replw);
+                                break;
+                            }
+                            if (v.indexOf(templ) !== -1) {
+                                if (typeof replw !== "string") {
+                                    throw new Error(
+                                        `cannot perform partial replacement w/ non-string '${replw}'`);
+                                }
+                                this.update(v.replace(`{{${k}}}`, replw));
+                            }
+                        }
+                    }
+                });
+            }
+
+            // Allocate the Pulumi resource, ensuring it depends on the prior node.
             const res = new (pulumi.CustomResource as any)(
                 getType(node), node.metadata.name, node, { dependsOn: prior && [ prior ] });
             prior = res;
